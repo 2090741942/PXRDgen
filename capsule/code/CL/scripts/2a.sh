efd100d231d0873d3c898cbc6b8b56889feba8d2
2090741942 <2090741942@qq.com>
2026-03-09 18:54:57 +0000

    path changed

---
+++ capsule/code/CL/scripts/2a.sh
@@ -0,0 +1,11 @@
+# python -u /workspace/PXRDgen/capsule/code/CL/scripts/test_all_accuracy.py --model_path /workspace/mp-20/outs/CL/CNN_t_0_05 --label -1
+# /workspace/mp-20/outs/CL/CNN_t_0_05/last_one.ckpt
+
+# python -u /workspace/PXRDgen/capsule/code/CL/scripts/test_all_accuracy.py --model_path /workspace/mp-20/outs/CL/CNN_t_1_00 --label -1
+# /workspace/mp-20/outs/CL/CNN_t_1_00/last_one.ckpt
+
+# python -u /workspace/PXRDgen/capsule/code/CL/scripts/test_all_accuracy.py --model_path /workspace/mp-20/outs/CL/T_t_0_05 --label -1
+# /workspace/mp-20/outs/CL/T_t_0_05/last_one.ckpt
+
+python -u /workspace/PXRDgen/capsule/code/CL/scripts/test_all_accuracy.py --model_path /workspace/mp-20/outs/CL/T_t_1_00 --label -1
+/workspace/mp-20/outs/CL/T_t_1_00/last_one.ckpt
---
+++ capsule/code/xrd2struc/scripts/compute_metrics.sh
@@ -0,0 +1,10 @@
+python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/compute.py \
+--pt_path /workspace/mp-20/outs/xrd2struc/flow_CNN/last_sample20_0.pt \
+--multi_eval
+
+python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/compute.py \
+--pt_path /workspace/mp-20/outs/xrd2struc/flow_CNN_L/last_sample1_0.pt
+
+python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/compute.py \
+--pt_path /workspace/mp-20/outs/xrd2struc/flow_CNN_L/last_sample20_0.pt \
+--multi_eval
