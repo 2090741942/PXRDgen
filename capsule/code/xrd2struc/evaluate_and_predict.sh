30ceb8626e4c2ac638b507fcb747eeeb59584972
2090741942 <2090741942@qq.com>
2026-04-04 13:28:29 +0000

    all train

--- capsule/code/xrd2struc/evaluate_and_predict.sh
+++ capsule/code/xrd2struc/evaluate_and_predict.sh
@@ -23,53 +23,53 @@
 # python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
 #   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_diffusion.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed --num_evals 20 --label -1 
 
 
-# Flow
+# # Flow
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed --num_evals 20 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed --num_evals 1 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed --num_evals 1 --label -1 
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed --num_evals 20 --label -1 
+# python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+#   --model_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed --num_evals 20 --label -1 
 
 
 
@@ -78,84 +78,84 @@ python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
 
 # python scripts/compute.py --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_unfixed/best_sample1_1.pt >> zout_train.txt
 
-echo "Diffusion" >> zout_train1.txt
+# echo "Diffusion" >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample20_0.pt  --multi_eval  >> zout_train1.txt 
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample20_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample20_0.pt  --multi_eval >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_unfixed/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_fixed/last_sample20_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_unfixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_unfixed/last_sample1_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-24/Diff_CNN_0.05_unfixed/last_sample20_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_unfixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_fixed/last_sample20_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL/last_sample20_0.pt --multi_eval >> zout_train1.txt
+
+# python scripts/compute.py \
+#   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
 
 python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_CNN_1.00_unfixed/last_sample20_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-25/Diff_T_0.05_fixed/last_sample20_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL/last_sample1_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_CNN_w_o_CL/last_sample20_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
-
-python scripts/compute.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed/last_sample20_0.pt  >> zout_train1.txt
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Diff_T_1.00_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
 
 # Flow
 
 echo "Flow" >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+python scripts/compute.py \
   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed/last_sample20_0.pt  >> zout_train1.txt
+python scripts/compute.py \
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_0.05_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+python scripts/compute.py \
   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed/last_sample1_0.pt  >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed/last_sample20_0.pt  >> zout_train1.txt
+python scripts/compute.py \
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_CNN_1.00_unfixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+python scripts/compute.py \
   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed/last_sample20_0.pt  >> zout_train1.txt
+python scripts/compute.py \
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-26/Flow_T_1.00_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+python scripts/compute.py \
   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed/last_sample1_0.pt  >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed/last_sample20_0.pt  >> zout_train1.txt
+python scripts/compute.py \
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_CNN_1.00_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
+python scripts/compute.py \
   --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed/last_sample1_0.pt  >> zout_train1.txt
 
-python /workspace/PXRDgen/capsule/code/xrd2struc/scripts/evaluate_flow.py \
-  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed/last_sample20_0.pt  >> zout_train1.txt
+python scripts/compute.py \
+  --pt_path /workspace/PXRDgen/capsule/code/xrd2struc/outputs/2026-03-28/Flow_T_0.05_fixed/last_sample20_0.pt --multi_eval >> zout_train1.txt
 
 
 
